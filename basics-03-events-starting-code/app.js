fc78f179ce36cbdb29d8e5f82e5de796212b6c94
Tanya Shukla <stanya97@gmail.com>
2021-10-03 19:41:20 +0530

    Native event object and event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,13 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:''
+      name:'',
+      confirmName:''
     };
   },
   methods:{
-    setName(event){ //we can accept this argument and you will always get this by  the browser.
+    confirmInput(){
+this.confirmName =this.name;
+    },
+    submitForm(event){
+      //this is a default browser mechanism buit in javascript
+      //and totally depemdent on vue
+      event.preventDefault();//this would tell browser that form should not be submitted that we dont want that browser default.
+      alert('submitted!!');
+    },
+    setName( event ,lastName){ //we can accept this argument and you will always get this by  the browser.
       //gives the information about event and  read the current value stored on that event
-      this.name = event.target.value;
+      this.name = event.target.value +' '+lastName;
     },
     add(num){ //we got it here also but we overwrote this with our own custom argument.
     this.counter = this.counter + num; //this method doesnt return anything,because we are using it to run it when a click occurs.
